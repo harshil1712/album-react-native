a467ec30ba776b9c5bd46b7857871ed485bb8a5a
Harshil Agrawal <ghagrawal17@gmail.com>
2018-04-20 00:38:25 +0530

    setup files

---
+++ index.js
@@ -0,0 +1,15 @@
+// import { AppRegistry } from 'react-native';
+// import App from './App';
+
+// AppRegistry.registerComponent('albums', () => App);
+
+import React from 'react';
+import { Text, AppRegistry } from 'react-native';
+
+const App = () =>{
+    return (
+        <Text>Some Text</Text>
+    );
+};
+
+AppRegistry.registerComponent('albums',()=>App);
