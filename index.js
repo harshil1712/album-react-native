de35cea03ac7ea663cb4c4ad750f0728e8c3d772
Harshil Agrawal <ghagrawal17@gmail.com>
2018-04-20 00:55:28 +0530

    header file created

--- index.js
+++ index.js
@@ -4,11 +4,12 @@
 // AppRegistry.registerComponent('albums', () => App);
 
 import React from 'react';
-import { Text, AppRegistry } from 'react-native';
+import { AppRegistry } from 'react-native';
+import Header from './src/components/header';
 
 const App = () =>{
     return (
-        <Text>Some Text</Text>
+        <Header/>
     );
 };
 
