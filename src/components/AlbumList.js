e3fb73e00f9671017a7e093bde9b22c471c64c9d
Harshil Agrawal <ghagrawal17@gmail.com>
2018-04-21 02:40:08 +0530

    card styled

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { View } from 'react-native';
+import { ScrollView } from 'react-native';
 import AlbumDetail from './AlbumDetail';
 import axios from 'axios';
 
@@ -17,9 +17,9 @@ class AlbumList extends Component {
 
     render(){
         return (
-            <View>
+            <ScrollView>
                 {this.renderAlbums()}
-            </View>
+            </ScrollView>
         )
     }
 }
